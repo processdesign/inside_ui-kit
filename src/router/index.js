907b1ec70a8f9c15d6090cfbfe6b3c9b288aa6e5
YAKorolev <korolev.ya.v@gmail.com>
2020-06-10 12:12:49 +0300

    Create TimeManagement-view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TimeManagement from '../views/TimeManagement.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,16 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
+
+  {
+    path: '/time',
+    name: 'TimeManagement',
+    component: TimeManagement
+  },
+
+
+
+
   {
     path: '/about',
     name: 'About',
